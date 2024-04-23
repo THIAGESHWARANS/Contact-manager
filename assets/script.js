b4048f373416bf1f39562eb4944e381d5f15932d
THIAGESHWARAN <sthiageshwaran@gmail.com>
2024-04-23 17:49:25 +0530

    added a script file.

--- assets/script.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var dropdowns = document.querySelectorAll('.dropdown-toggle');
-    dropdowns.forEach(function(dropdown) {
-        new bootstrap.Dropdown(dropdown);
-    });
-});
